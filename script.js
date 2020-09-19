3378348e3a5d2b5b72eec6d52096add0949dad3d
Awa <francisawa2@gmail.com>
2020-09-18 18:12:21 -0700

    first commit of password generator

---
+++ script.js
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////
+// PASSWORD GENERATOR
+//
+// * For this assignment, you will not be changing the HTML and CSS at all.
+//
+// * You will need a generatePassword function is called when the user
+//   clicks the Generate Password button.
+//
+// * You can create other functions that are called from within
+//   generatePassword
+//
+// * Gather user input with prompt's and confirm's
+
+///////////////////////////////////////////////////////////////////////
+// DO NOT TOUCH THIS CODE
+//
+// This code handles:
+// * clicking the Generate Password
+// * writing the password to the screen
+//
+var numberChars = ['0','1','2','3','4','5','6','7','8','9']
+var lowerChars = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+var upperChars = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+var specialChars = [
+  '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
+];
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////////////////////
+function generatePassword(){
+  
+}
